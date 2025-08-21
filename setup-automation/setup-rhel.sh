6b0674d9b5c5c38b3f03ec1f87bc099f7f2444f8
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-08-21 13:54:55 -0400

    add user to the weldr group

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -6,6 +6,9 @@ touch /root/post-run.log
 # Create a done file to signal we have finished
 touch /root/post-run.log.done
 
+# add user to the weldr group
+sudo usermod -aG weldr rhel
+
 tee -a /home/rhel/node-app-server.toml << EOF
 name = "node-app-server"
 description = "Sample image for the node.js application server"
