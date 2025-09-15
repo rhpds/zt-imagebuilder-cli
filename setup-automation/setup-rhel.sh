ba24c15888d228dfe1754dae8b3bcdd8a874be90
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-09-15 08:55:16 -0400

    Revert "changed scripting output directory"
    This reverts commit a6307bdbed7cbc0b52db70b78d6ed7fae3886dc9.

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -6,7 +6,7 @@ touch /root/post-run.log
 # Create a done file to signal we have finished
 touch /root/post-run.log.done
 
-tee -a /home/rhel/node-app-server.toml << EOF
+tee -a /root/node-app-server.toml << EOF
 name = "node-app-server"
 description = "Sample image for the node.js application server"
 version = "0.0.1"
